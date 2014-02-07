8f39ade9cf7a123c35ad517b6e01a22a2bb8f242
Nick Lothian <nick.lothian@gmail.com>
2014-02-07 14:09:21 +1030

    port

--- services/quepy-service/webservice.py
+++ services/quepy-service/webservice.py
@@ -38,6 +38,5 @@ def hello():
 	return "ok"
 
 if __name__ == "__main__":
-    #app.run(host="0.0.0.0", port=5001, debug=True)
-    app.run(port=5001, debug=True)
+    app.run(host="0.0.0.0", port=5001, debug=True)
 

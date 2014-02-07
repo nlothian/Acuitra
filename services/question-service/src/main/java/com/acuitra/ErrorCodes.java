36c416f06635dac5b27fc1cf3d5ce79d4f3a251f
Nick Lothian <nick.lothian@gmail.com>
2014-02-07 14:06:41 +1100

    Better error handling

---
+++ services/question-service/src/main/java/com/acuitra/ErrorCodes.java
@@ -0,0 +1,15 @@
+package com.acuitra;
+
+/**
+ * Add a page on https://github.com/nlothian/Acuitra/wiki/_pages in the format "Error: <error code string below>";
+ * 
+ * @author nlothian
+ *
+ */
+public class ErrorCodes {
+	
+	public final static String INVALID_RESOURCE = "INVALID_RESOURCE";
+	public static final String INTERNAL_ERROR = "INTERNAL_ERROR";
+	public static final String COULD_NOT_UNDERSTAND_QUESTION = "COULD_NOT_UNDERSTAND_QUESTION";
+
+}

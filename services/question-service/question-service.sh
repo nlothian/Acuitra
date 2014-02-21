e91603df9da7e47dd2cea9aa0e5454cbf17bbf99
azure provisioned user <azureuser@acuitra1.acuitra.d3.internal.cloudapp.net>
2014-02-21 09:08:56 +0000

    config

--- services/question-service/question-service.sh
+++ services/question-service/question-service.sh
@@ -32,10 +32,9 @@ run() {
     if [  "$QUEPY_PORT_5001_TCP_PROTO" ] ; then
         JAVA_OPTS="$JAVA_OPTS "-Ddw.quepyURL=http://"$QUEPY_PORT_5001_TCP_ADDR":"$QUEPY_PORT_5001_TCP_PORT"/question
     fi
-    if [  "$NAMED_ENTITY_SERVICE_5000_TCP_PROTO" ] ; then
-        JAVA_OPTS="$JAVA_OPTS "-Ddw.namedEntityRecognitionURL=http://"$NAMED_ENTITY_SERVICE_5000_TCP_ADDR":"$NAMED_ENTITY_SERVICE_5000_TCP_PORT"/ner
+    if [  "$NAMED_ENTITY_SERVICE_PORT_5000_TCP_PROTO" ] ; then
+        JAVA_OPTS="$JAVA_OPTS "-Ddw.namedEntityRecognitionURL=http://"$NAMED_ENTITY_SERVICE_PORT_5000_TCP_ADDR":"$NAMED_ENTITY_SERVICE_PORT_5000_TCP_PORT"/ner
     fi
-
 	java $JAVA_OPTS -jar ./target/acuitra-0.0.1-SNAPSHOT.jar server question-service.yaml
 }
 
